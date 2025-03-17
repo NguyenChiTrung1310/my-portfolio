f08a072e8b805234070c66865e4e5b33d69113e7
Trung Nguyen <nguyenchitrung1310@gmail.com>
2025-03-17 07:35:33 +0700

    Update transition

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,16 +1,32 @@
-import React, {PropsWithChildren} from 'react'
+'use client'
+import {AnimatePresence, motion} from 'motion/react'
+import {usePathname} from 'next/navigation'
+import React, {PropsWithChildren, useEffect, useState} from 'react'
 
 import Header from '@/components/Header'
 import Navbar from '@/components/Navbar'
 import TopLeftImg from '@/components/TopLeftImg'
+import Transition from '@/components/Transition'
 
 const Layout: React.FC<PropsWithChildren> = ({children}) => {
+  const pathname = usePathname()
+  const [showChildren, setShowChildren] = useState<boolean>(false)
+
+  useEffect(() => {
+    setShowChildren(false) // Hide page while transition runs
+  }, [pathname])
+
   return (
     <div className='page bg-site relative bg-cover bg-no-repeat text-white'>
       <TopLeftImg />
       <Navbar />
       <Header />
-      {children}
+      <AnimatePresence mode='wait'>
+        <motion.div key={pathname} className='h-full'>
+          <Transition onComplete={setShowChildren} />
+          {showChildren && children}
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
