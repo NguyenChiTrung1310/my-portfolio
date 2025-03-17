af65ee90d612167837a0d3f4893c7a4149e318ee
Trung Nguyen <chitrung.nguyen@vietlink.jp>
2025-03-17 09:43:23 +0700

    Build UI page Home

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <>OKOK</>
+  return (
+    <section className='bg-primary/60 h-full'>
+      <div className='from-primary/10 h-full w-full bg-gradient-to-r via-black/30 to-black/10'>
+        <div className='container mx-auto flex h-full flex-col justify-center text-center xl:pt-40 xl:text-left'>
+          <h1>
+            Hello. I am <span className='text-accent'>Trung Nguyen</span>
+          </h1>
+          <div className='mx-auto mb-10 max-w-sm xl:mx-0 xl:mb-16 xl:max-w-xl'>I&apos;m a Frontend Developer</div>
+        </div>
+      </div>
+    </section>
+  )
 }
