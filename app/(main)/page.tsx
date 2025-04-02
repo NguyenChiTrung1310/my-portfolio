4b5d767d765f43024899ddb5090361b11237221c
Trung Nguyen <chitrung.nguyen@vietlink.jp>
2025-04-02 17:32:22 +0700

    Custom three component

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,14 @@
+'use client'
+import GridDistortion from '@/components/Hero'
 import Curve from '@/components/Transitions/Curve'
 
 export default function Home() {
   return (
     <Curve>
-      <div className='h-screen'>Home</div>
+      <div className='relative h-screen'>
+        Home
+        <GridDistortion imageSrc='https://picsum.photos/1920/1080?grayscale' grid={33} mouse={0.1} strength={0.15} relaxation={0.9} className='custom-class' />
+      </div>
     </Curve>
   )
 }
