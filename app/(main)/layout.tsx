6e9df0cbbccfc3eab905b2b303e2814ff8a19fb8
Trung Nguyen <nguyenchitrung1310@gmail.com>
2025-04-05 15:48:58 +0700

    Update

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,11 +3,15 @@ import React, {PropsWithChildren} from 'react'
 
 import PageAnimatePresence from '@/components/HOC/PageAnimatePresence'
 import Navbar from '@/components/Navbar'
+import SpotlightCursor from '@/components/SpotlightCursor'
 
 const Layout: React.FC<PropsWithChildren> = ({children}) => {
   return (
     <div>
       <Navbar />
+      <div className='absolute -z-10'>
+        <SpotlightCursor />
+      </div>
       <PageAnimatePresence>{children}</PageAnimatePresence>
     </div>
   )
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import SpotlightCursor from '@/components/SpotlightCursor'
 import Curve from '@/components/Transitions/Curve'
 
 export default function Home() {
@@ -7,9 +6,6 @@ export default function Home() {
     <Curve>
       <div className='relative'>
         <div className='h-screen'>Hoh-screenme</div>
-        <div className='absolute -z-10'>
-          <SpotlightCursor />
-        </div>
         <div className='h-screen'>AB BABABAB</div>
       </div>
     </Curve>
